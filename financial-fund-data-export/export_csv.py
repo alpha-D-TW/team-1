e06a402682f977d1d2835561c98933cd5f76adf1
rexerwang <rexercn@gmail.com>
2024-02-27 15:41:09 +0800

    feat: export chunk txt

--- financial-fund-data-export/export_csv.py
+++ financial-fund-data-export/export_csv.py
@@ -6,33 +6,39 @@ import csv
 with open('data.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-# 随机打乱基金数据
-random.shuffle(data)
+# 对基金数据按照 risk 排序
+data.sort(key=lambda x: x['risk'])
 
-# 存储所有基金信息的列表
-funds = []
+# 存储已导出的基金数量，按照 risk 类型记录
+exported_counts = {'R1': 0, 'R2': 0, 'R3': 0, 'R4': 0, 'R5': 0}
 
+# 存储导出的基金信息
+exported_funds = []
+
+# 遍历基金数据，按照要求选择导出的数据
 for fund in data:
     risk = fund['risk']
-    annual_return_rate = round(random.uniform(0, 100), 4)  # 生成随机年度回报率
-    fund_data = {
-        'name': fund['name'],
-        'shortName': fund['shortName'],
-        'code': fund['code'],
-        'risk': fund['risk'],
-        'ForhbinfoUrl': fund['ForhbinfoUrl'],
-        'annualReturnRate': round(annual_return_rate / 100, 2)
-    }
-    funds.append(fund_data)
+    if exported_counts[risk] < 5:  # 检查该风险等级已导出的基金数量是否已经达到了 5 条
+        annual_return_rate = round(random.uniform(0, 100), 4)  # 生成随机年度回报率
+        fund_data = {
+            'name': fund['name'],
+            'shortName': fund['shortName'],
+            'code': fund['code'],
+            'risk': risk,
+            'ForhbinfoUrl': fund['ForhbinfoUrl'],
+            'annualReturnRate': round(annual_return_rate / 100, 2)
+        }
+        exported_funds.append(fund_data)
+        exported_counts[risk] += 1  # 更新已导出的基金数量
 
-# 将所有基金信息导出到 CSV 文件中
+# 将导出的基金信息导出到 CSV 文件中
 csv_columns = ['name', 'shortName', 'code', 'risk', 'ForhbinfoUrl', 'annualReturnRate']
 csv_file = 'dist/funds.csv'
 
 with open(csv_file, 'w', newline='', encoding='utf-8') as file:
     writer = csv.DictWriter(file, fieldnames=csv_columns)
     writer.writeheader()
-    for fund in funds:
+    for fund in exported_funds:
         writer.writerow(fund)
 
 print("Generated CSV file:", csv_file)
--- financial-fund-data-export/export_preview.py
+++ financial-fund-data-export/export_preview.py
@@ -12,6 +12,6 @@ for item in data:
         risk_end_index = page_content.find('\n', risk_start_index)
         if risk_end_index != -1:
             risk = page_content[risk_start_index + len('risk:'):risk_end_index].strip()
-            txt_filename = f"dist/{risk}_data.txt"
+            txt_filename = f"dist/{risk}.txt"
             with open(txt_filename, 'a', encoding='utf-8') as txt_file:
-                txt_file.write(page_content.replace('\n', ' ') + '\n')
+                txt_file.write('"' + page_content.replace('\n', '\\n') + '"' + '\n')
